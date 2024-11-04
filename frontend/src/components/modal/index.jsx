76e70ad25e790c63a458586617d2e1d87cabd0a8
ino_ermes <hikigayahachiman.akame.kill@gmail.com>
2024-11-04 22:22:08 +0700

    Add post form

---
+++ frontend/src/components/modal/index.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import './index.scss';
+
+const Modal = ({ children, onClickOutside }) => {
+  const onClickExceptChild = (e) => {
+    if (e.currentTarget !== e.target) return;
+    if (onClickOutside) onClickOutside();
+  };
+
+  useEffect(() => {
+    document.body.style.overflowY = 'hidden';
+    return () => {
+      document.body.style.overflowY = 'scroll';
+    };
+  }, []);
+
+  return (
+    <aside className='modal' onMouseDown={onClickExceptChild}>
+      {children}
+    </aside>
+  );
+};
+
+export default Modal;
--- frontend/src/views/pages/individual/index.jsx
+++ frontend/src/views/pages/individual/index.jsx
@@ -1,19 +1,22 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import "./index.scss";
-import { AuthContext } from "../../../context/authContext";
-import { Button } from "antd";
-import { CameraFilled } from "@ant-design/icons";
-import TextArea from "antd/es/input/TextArea";
-import { ImGrin } from "react-icons/im";
-import SmallPost from "./SmallPost";
-import { FaRegImage } from "react-icons/fa";
-import PostAPI from "../../../api/postAPI";
-import AccountAPI from "../../../api/accountAPI";
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import './index.scss';
+import { AuthContext } from '../../../context/authContext';
+import { Button } from 'antd';
+import { CameraFilled } from '@ant-design/icons';
+import TextArea from 'antd/es/input/TextArea';
+import { ImGrin } from 'react-icons/im';
+import SmallPost from './SmallPost';
+import { FaRegImage } from 'react-icons/fa';
+import PostAPI from '../../../api/postAPI';
+import AccountAPI from '../../../api/accountAPI';
+import Modal from '../../../components/modal';
+import AddPostForm from './AddPostForm';
 
 function Individual() {
   const { user, setUser } = useContext(AuthContext);
-  const [newPost, setNewPost] = useState("");
+  const [newPost, setNewPost] = useState('');
   const [posts, setPosts] = useState([]);
+  const [showAddForm, setShowAddForm] = useState(false);
 
   // Refs for file inputs
   const avatarInputRef = useRef(null);
@@ -65,8 +68,8 @@ function Individual() {
     const file = event.target.files[0];
     if (file) {
       const formData = new FormData();
-      formData.append("image", file);
-      formData.append("userId", user.userId);
+      formData.append('image', file);
+      formData.append('userId', user.userId);
       AccountAPI.updateAvatar(formData)
         .then((res) => {
           console.log(res.data.data);
@@ -83,8 +86,8 @@ function Individual() {
     const file = event.target.files[0];
     if (file) {
       const formData = new FormData();
-      formData.append("bgImage", file);
-      formData.append("userId", user.userId);
+      formData.append('bgImage', file);
+      formData.append('userId', user.userId);
       AccountAPI.updateBackground(formData)
         .then((res) => {
           console.log(res.data.data);
@@ -100,82 +103,119 @@ function Individual() {
     setNewPost(value.target.value);
   };
 
+  const handleAddLocalPost = (post) => {
+    setPosts((posts) => [
+      {
+        ...post,
+        id: post.id,
+        content: post.content,
+        image: post.image,
+        likeNum: post.likes?.length || 0,
+        commentNum: post.comments?.length || 0,
+        comments: post.comments
+          ? post.comments.map((comment) => ({
+              id: comment.id,
+              content: comment.content,
+              account: comment.user,
+            }))
+          : [],
+      },
+      ...posts,
+    ]);
+  };
+
+  const handleCloseAddForm = () => {
+    setShowAddForm(false);
+  };
+  const handleShowAddForm = () => {
+    setShowAddForm(true);
+  };
+
   return (
-    <div className="individual-container">
-      <div className="individual-info-container">
-        <div className="individual-background-container">
+    <div className='individual-container'>
+      {showAddForm && (
+        <Modal onClickOutside={handleCloseAddForm}>
+          <AddPostForm
+            handleAddLocalPost={handleAddLocalPost}
+            handleCloseForm={handleCloseAddForm}
+          />
+        </Modal>
+      )}
+      <div className='individual-info-container'>
+        <div className='individual-background-container'>
           <img
             src={user.backgroundImage}
-            alt="user background"
-            className="individual-info-background"
+            alt='user background'
+            className='individual-info-background'
           />
           <Button
             onClick={onChangeBackground}
-            className="individual-info-background-button"
+            className='individual-info-background-button'
           >
             <p>Chỉnh sửa ảnh bìa</p>
             <CameraFilled />
           </Button>
           {/* Hidden file input for background */}
           <input
-            type="file"
-            accept="image/*"
+            type='file'
+            accept='image/*'
             ref={backgroundInputRef}
-            style={{ display: "none" }}
+            style={{ display: 'none' }}
             multiple={false}
             onChange={handleBackgroundChange}
           />
         </div>
-        <div className="individual-info-persional-container">
-          <div className="position-relative">
+        <div className='individual-info-persional-container'>
+          <div className='position-relative'>
             <img
               src={user.avatar}
-              alt="user avatar"
-              className="individual-info-avatar"
+              alt='user avatar'
+              className='individual-info-avatar'
             />
             <Button
               onClick={onChangeAvatar}
-              className="individual-info-avatar-button"
+              className='individual-info-avatar-button'
             >
-              <CameraFilled style={{ fontSize: "25px" }} />
+              <CameraFilled style={{ fontSize: '25px' }} />
             </Button>
             {/* Hidden file input for avatar */}
             <input
-              type="file"
-              accept="image/*"
+              type='file'
+              accept='image/*'
               ref={avatarInputRef}
-              style={{ display: "none" }}
+              style={{ display: 'none' }}
               multiple={false}
               onChange={handleAvatarChange}
             />
           </div>
-          <p className="individual-username">{user.username}</p>
+          <p className='individual-username'>{user.username}</p>
         </div>
       </div>
-      <div className="individual-new-post-container">
+      <div className='individual-new-post-container'>
         <img
-          alt="user avatar"
+          alt='user avatar'
           src={user.avatar}
-          className="individual-new-post-avatar"
+          className='individual-new-post-avatar'
         />
-        <div className="individual-new-post-input-container">
+        <div className='individual-new-post-input-container'>
           <TextArea
-            placeholder="Bạn đang nghĩ gì ?"
-            className="individual-new-post-input"
+            placeholder='Bạn đang nghĩ gì ?'
+            className='individual-new-post-input'
             autoSize
             value={newPost}
             onChange={onChangeNewPost}
+            onClick={handleShowAddForm}
           />
-          <div className="individual-new-post-button-container">
+          <div className='individual-new-post-button-container'>
             <Button
               icon={<FaRegImage />}
-              className="individual-new-post-button"
+              className='individual-new-post-button'
             />
-            <Button icon={<ImGrin />} className="individual-new-post-button" />
+            <Button icon={<ImGrin />} className='individual-new-post-button' />
           </div>
         </div>
       </div>
-      <div className="individual-posts-container">
+      <div className='individual-posts-container'>
         {posts &&
           posts.length > 0 &&
           posts.map((post) => <SmallPost post={post} user={user} />)}
