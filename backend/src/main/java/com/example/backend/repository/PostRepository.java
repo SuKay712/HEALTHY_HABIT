9bb0d8c87fbfe3feb235e58ba97b324fb87565d5
Võ Viết Trường <voviettruong141003@gmail.com>
2024-11-20 19:14:45 +0700

    fix saved post

--- backend/src/main/java/com/example/backend/repository/PostRepository.java
+++ backend/src/main/java/com/example/backend/repository/PostRepository.java
@@ -13,55 +13,88 @@ import com.example.backend.model.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, ObjectId> {
 
-    @Aggregation(pipeline = {
-            "{ '$match': { 'userId': { '$eq': ?0 } } }",
-            "{ '$lookup': { 'from': 'Users', 'localField': 'userId', 'foreignField': '_id', 'as': 'postUser' } }",
-            "{ '$addFields': { 'postUser': { '$arrayElemAt': ['$postUser', 0] } } }",
-            "{ '$lookup': { 'from': 'Comments', 'localField': '_id', 'foreignField': 'postId', 'as': 'comments' } }",
-            "{ '$lookup': { 'from': 'Users', 'localField': 'comments.userId', 'foreignField': '_id', 'as': 'commentUser' } }",
-            "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'user': { '$arrayElemAt': ['$commentUser', 0] } }] } } } } } }",
-            "{ '$addFields': { 'hasLiked': { '$in': [ { '$toString': ?0 }, '$likes' ] } } }",
-            "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'hasLikedComment': { '$in': [ { '$toString': ?0 }, '$$comment.likes' ] } }] } } } } } }",
-            "{ '$lookup': { 'from': 'Posts', 'localField': 'savedPost', 'foreignField': '_id', 'as': 'savedPosts' } }",
-            "{ '$sort': { 'createdAt': -1 } }",
-            "{ '$project': { 'id': 1, 'userId': 1, 'content': 1, 'image': 1, 'inTrashcan': 1, 'isDeleted': 1, 'likes': 1, 'createdAt': 1, 'updatedAt': 1, 'comments': { '$cond': [{ '$gt': [{ '$size': '$comments' }, 0] }, '$comments', []] }, 'hasLiked': 1, 'postUser': 1, 'savedPosts': 1 } }"
-    })
-    List<Post> findPostsWithCommentsByUserId(ObjectId userId);
+        @Aggregation(pipeline = {
+                        "{ '$match': { 'userId': { '$eq': ?0 } } }",
+                        "{ '$lookup': { 'from': 'Users', 'localField': 'userId', 'foreignField': '_id', 'as': 'postUser' } }",
+                        "{ '$addFields': { 'postUser': { '$arrayElemAt': ['$postUser', 0] } } }",
+                        "{ '$lookup': { 'from': 'Comments', 'localField': '_id', 'foreignField': 'postId', 'as': 'comments' } }",
+                        "{ '$lookup': { 'from': 'Users', 'localField': 'comments.userId', 'foreignField': '_id', 'as': 'commentUser' } }",
+                        "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'user': { '$arrayElemAt': ['$commentUser', 0] } }] } } } } } }",
+                        "{ '$addFields': { 'hasLiked': { '$in': [ { '$toString': ?0 }, '$likes' ] } } }",
+                        "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'hasLikedComment': { '$in': [ { '$toString': ?0 }, '$$comment.likes' ] } }] } } } } } }",
+                        "{ '$lookup': { 'from': 'Posts', 'localField': 'savedPost', 'foreignField': '_id', 'as': 'savedPosts' } }",
+                        "{ '$sort': { 'createdAt': -1 } }",
+                        "{ '$project': { 'id': 1, 'userId': 1, 'content': 1, 'image': 1, 'inTrashcan': 1, 'isDeleted': 1, 'likes': 1, 'createdAt': 1, 'updatedAt': 1, 'comments': { '$cond': [{ '$gt': [{ '$size': '$comments' }, 0] }, '$comments', []] }, 'hasLiked': 1, 'postUser': 1, 'savedPosts': 1 } }"
+        })
+        List<Post> findPostsWithCommentsByUserId(ObjectId userId);
 
-    @Aggregation(pipeline = {
-            "{ '$lookup': { 'from': 'Comments', 'localField': '_id', 'foreignField': 'postId', 'as': 'comments' } }",
-            "{ '$lookup': { 'from': 'Users', 'localField': 'comments.userId', 'foreignField': '_id', 'as': 'commentUser' } }",
-            "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'user': { '$arrayElemAt': ['$commentUser', 0] } }] } } } } } }",
-            "{ '$lookup': { 'from': 'Users', 'localField': 'userId', 'foreignField': '_id', 'as': 'postUser' } }",
-            "{ '$addFields': { 'postUser': { '$arrayElemAt': ['$postUser', 0] } } }",
-            "{ '$sort': { 'createdAt': -1 } }",
-            "{ '$skip': ?0 }",
-            "{ '$limit': ?1 }",
-            "{ '$project': { 'id': 1, 'userId': 1, 'content': 1, 'image': 1, 'inTrashcan': 1, 'isDeleted': 1, 'likes': 1, 'createdAt': 1, 'updatedAt': 1, 'comments': { '$cond': [{ '$gt': [{ '$size': '$comments' }, 0] }, '$comments', []] }, 'postUser': 1 } }"
-    })
-    List<Post> findAllPostsWithComments(int skip, int limit);
+        @Aggregation(pipeline = {
+                        "{ '$match': { '_id': { '$in': ?0 } } }", // Lấy các bài viết có _id nằm trong savedPost
+                        "{ '$lookup': { 'from': 'Users', 'localField': 'userId', 'foreignField': '_id', 'as': 'postUser' } }", // Thêm
+                                                                                                                               // thông
+                                                                                                                               // tin
+                                                                                                                               // người
+                                                                                                                               // dùng
+                        "{ '$lookup': { 'from': 'Comments', 'localField': '_id', 'foreignField': 'postId', 'as': 'comments' } }", // Thêm
+                                                                                                                                  // thông
+                                                                                                                                  // tin
+                                                                                                                                  // bình
+                                                                                                                                  // luận
+                        "{ '$addFields': { 'hasLiked': { '$in': [ { '$toString': ?1 }, '$likes' ] } } }", // Kiểm tra
+                                                                                                          // xem userId
+                                                                                                          // có thích
+                                                                                                          // bài viết
+                                                                                                          // hay không
+                        "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'user': { '$arrayElemAt': ['$commentUser', 0] } }] } } } } }", // Thêm
+                                                                                                                                                                                                                  // thông
+                                                                                                                                                                                                                  // tin
+                                                                                                                                                                                                                  // người
+                                                                                                                                                                                                                  // dùng
+                                                                                                                                                                                                                  // vào
+                                                                                                                                                                                                                  // comments
+                        "{ '$project': { 'id': 1, 'userId': 1, 'content': 1, 'image': 1, 'inTrashcan': 1, 'isDeleted': 1, 'likes': 1, 'createdAt': 1, 'updatedAt': 1, 'comments': { '$cond': [{ '$gt': [{ '$size': '$comments' }, 0] }, '$comments', []] }, 'hasLiked': 1, 'postUser': { '$arrayElemAt': ['$postUser', 0] } } }" // Chỉ
+                                                                                                                                                                                                                                                                                                                                 // giữ
+                                                                                                                                                                                                                                                                                                                                 // các
+                                                                                                                                                                                                                                                                                                                                 // trường
+                                                                                                                                                                                                                                                                                                                                 // cần
+                                                                                                                                                                                                                                                                                                                                 // thiết
+        })
+        List<Post> findPostsBySavedPostAndUserId(List<ObjectId> savedPost, ObjectId userId);
 
-    @Query("{ 'isDeleted': false }")
-    List<Post> findAllPosts();
+        @Aggregation(pipeline = {
+                        "{ '$lookup': { 'from': 'Comments', 'localField': '_id', 'foreignField': 'postId', 'as': 'comments' } }",
+                        "{ '$lookup': { 'from': 'Users', 'localField': 'comments.userId', 'foreignField': '_id', 'as': 'commentUser' } }",
+                        "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'user': { '$arrayElemAt': ['$commentUser', 0] } }] } } } } } }",
+                        "{ '$lookup': { 'from': 'Users', 'localField': 'userId', 'foreignField': '_id', 'as': 'postUser' } }",
+                        "{ '$addFields': { 'postUser': { '$arrayElemAt': ['$postUser', 0] } } }",
+                        "{ '$sort': { 'createdAt': -1 } }",
+                        "{ '$skip': ?0 }",
+                        "{ '$limit': ?1 }",
+                        "{ '$project': { 'id': 1, 'userId': 1, 'content': 1, 'image': 1, 'inTrashcan': 1, 'isDeleted': 1, 'likes': 1, 'createdAt': 1, 'updatedAt': 1, 'comments': { '$cond': [{ '$gt': [{ '$size': '$comments' }, 0] }, '$comments', []] }, 'postUser': 1 } }"
+        })
+        List<Post> findAllPostsWithComments(int skip, int limit);
 
-    @Aggregation(pipeline = {
-            "{ '$lookup': { 'from': 'Comments', 'localField': '_id', 'foreignField': 'postId', 'as': 'comments' } }",
+        @Query("{ 'isDeleted': false }")
+        List<Post> findAllPosts();
 
-            "{ '$lookup': { 'from': 'Users', 'localField': 'comments.userId', 'foreignField': '_id', 'as': 'commentUser' } }",
+        @Aggregation(pipeline = {
+                        "{ '$lookup': { 'from': 'Comments', 'localField': '_id', 'foreignField': 'postId', 'as': 'comments' } }",
 
-            "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'user': { '$arrayElemAt': ['$commentUser', 0] } }] } } } } }",
+                        "{ '$lookup': { 'from': 'Users', 'localField': 'comments.userId', 'foreignField': '_id', 'as': 'commentUser' } }",
 
-            "{ '$lookup': { 'from': 'Users', 'localField': 'userId', 'foreignField': '_id', 'as': 'postUser' } }",
+                        "{ '$addFields': { 'comments': { '$map': { 'input': '$comments', 'as': 'comment', 'in': { '$mergeObjects': ['$$comment', { 'user': { '$arrayElemAt': ['$commentUser', 0] } }] } } } } }",
 
-            "{ '$addFields': { 'likeCount': { '$size': { '$ifNull': ['$likes', []] } }, 'commentCount': { '$size': { '$ifNull': ['$comments', []] } } } }",
-            "{ '$sort': { 'likeCount': -1, 'commentCount': -1 } }",
-            "{ '$project': { " +
-                    "'id': 1, 'userId': 1, 'content': 1, 'image': 1, 'inTrashcan': 1, 'isDeleted': 1, 'likes': 1, 'createdAt': 1, 'updatedAt': 1, "
-                    +
-                    "'postUser': { '$arrayElemAt': ['$postUser', 0] }, " +
-                    "'comments': { '$cond': [{ '$gt': [{ '$size': '$comments' }, 0] }, '$comments', []] } } }"
+                        "{ '$lookup': { 'from': 'Users', 'localField': 'userId', 'foreignField': '_id', 'as': 'postUser' } }",
 
-    })
-    List<Post> findAllPostsWithCommentsSortedByLikesAndComments();
+                        "{ '$addFields': { 'likeCount': { '$size': { '$ifNull': ['$likes', []] } }, 'commentCount': { '$size': { '$ifNull': ['$comments', []] } } } }",
+                        "{ '$sort': { 'likeCount': -1, 'commentCount': -1 } }",
+                        "{ '$project': { " +
+                                        "'id': 1, 'userId': 1, 'content': 1, 'image': 1, 'inTrashcan': 1, 'isDeleted': 1, 'likes': 1, 'createdAt': 1, 'updatedAt': 1, "
+                                        +
+                                        "'postUser': { '$arrayElemAt': ['$postUser', 0] }, " +
+                                        "'comments': { '$cond': [{ '$gt': [{ '$size': '$comments' }, 0] }, '$comments', []] } } }"
+
+        })
+        List<Post> findAllPostsWithCommentsSortedByLikesAndComments();
 
 }
