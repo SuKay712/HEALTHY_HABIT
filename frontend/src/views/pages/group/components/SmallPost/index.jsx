4f9a5a3f16647ff0ee1a9d83a7b3e07efb5ad23e
ducbinh <ducbinh120701@gmail.com>
2024-11-20 18:27:53 +0700

    fix:sprint3_no7

--- frontend/src/views/pages/group/components/SmallPost/index.jsx
+++ frontend/src/views/pages/group/components/SmallPost/index.jsx
@@ -108,11 +108,15 @@ function SmallPost(props) {
           <img
             className="group-small-post-avatar"
             alt="user avatar"
-            src={user.avatar}
+            src={post.postUser?.avatar ? post.postUser.avatar : "#"}
           />
           <div className="group-small-post-info-container">
             <div className="d-flex align-items-center">
-              <p class="group-small-post-username">{user.displayName}</p>
+              <p class="group-small-post-username">
+                {post.postUser?.displayName
+                  ? post.postUser.displayName
+                  : "Empty User"}
+              </p>
               <p class="group-small-post-status">
                 <FaCircle />
               </p>
@@ -164,9 +168,24 @@ function SmallPost(props) {
           <Button
             icon={
               isPin ? (
-                <PushpinFilled style={{ fontSize: "25px", color: "#ebc351" }} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                  fill="#ebc351"
+                  width="22px"
+                  height="22px"
+                >
+                  <path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z" />
+                </svg>
               ) : (
-                <PushpinOutlined style={{ fontSize: "25px" }} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                  width="22px"
+                  height="22px"
+                >
+                  <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z" />
+                </svg>
               )
             }
             className="group-small-post-button"
@@ -177,7 +196,7 @@ function SmallPost(props) {
           <div className="group-small-post-comment-container">
             <img
               alt="user avatar"
-              src={user.avatar}
+              src={post.postUser?.avatar ? post.postUser.avatar : "#"}
               className="group-small-post-avatar"
             />
             <TextArea
